00546cf272c91a721d263334c2e3ddcd0ffdefe8
Pedro Cornejo <pedrocornejo99@gmail.com>
2024-02-14 23:04:13 -0300

    first commit

---
+++ app/components/card/card.tsx
@@ -0,0 +1,38 @@
+export default function Card() {
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="box">
+                    <div className="content">
+                        <h2>01</h2>
+                        <h3>Mundo</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, placeat quia. Nam quae atque mollitia ipsam esse inventore nostrum a temporibus dolorem molestias! Natus vel aliquid voluptatibus ullam, voluptate deleniti.</p>
+                        <a href="#">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="box">
+                    <div className="content">
+                        <h2>02</h2>
+                        <h3>Mundo</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, placeat quia. Nam quae atque mollitia ipsam esse inventore nostrum a temporibus dolorem molestias! Natus vel aliquid voluptatibus ullam, voluptate deleniti.</p>
+                        <a href="#">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="box">
+                    <div className="content">
+                        <h2>03</h2>
+                        <h3>Mundo</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, placeat quia. Nam quae atque mollitia ipsam esse inventore nostrum a temporibus dolorem molestias! Natus vel aliquid voluptatibus ullam, voluptate deleniti.</p>
+                        <a href="#">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
